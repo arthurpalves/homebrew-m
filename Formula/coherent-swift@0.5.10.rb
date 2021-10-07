8a43cacffcf945909066d265fa9faaa2168dc53d
Arthur Alves <arthuralves.p@gmail.com>
2021-10-07 11:59:21 +0200

    chore: correct formulas

--- Formula/coherent-swift@0.5.10.rb
+++ Formula/coherent-swift@0.5.10.rb
@@ -1,5 +1,5 @@
 
-class CoherentSwift10 < Formula
+class CoherentSwiftAT0510 < Formula
   desc "A command-line tool that measures cohesion in your Swift codebase"
   homepage "https://github.com/arthurpalves/coherent-swift.git"
   url "https://github.com/arthurpalves/coherent-swift.git", tag: "0.5.10"
--- Formula/coherent-swift@0.5.9.rb
+++ Formula/coherent-swift@0.5.9.rb
@@ -1,5 +1,5 @@
 
-class CoherentSwift059 < Formula
+class CoherentSwiftAT059 < Formula
   desc "A command-line tool that measures cohesion in your Swift codebase"
   homepage "https://github.com/arthurpalves/coherent-swift.git"
   url "https://github.com/arthurpalves/coherent-swift.git", tag: "0.5.9"
