74cdc1aa6a997cb1de486c925a4a8cca3f6c940f
Arthur Alves <arthuralves.p@gmail.com>
2021-10-07 11:52:09 +0200

    coherent-swift release 0.5.10

---
+++ Formula/coherent-swift@0.5.9.rb
@@ -0,0 +1,13 @@
+
+class CoherentSwift059 < Formula
+  desc "A command-line tool that measures cohesion in your Swift codebase"
+  homepage "https://github.com/arthurpalves/coherent-swift.git"
+  url "https://github.com/arthurpalves/coherent-swift.git", tag: "0.5.9"
+  version "0.5.9"
+
+  depends_on :macos
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+end
